ad1b303cec9f2cd72087d4f3c7e5f0660c114e78
IogaMaster <iogamastercode@gmail.com>
2022-06-28 20:39:54 -0600

    Initial Commit :tada:

---
+++ src/main/java/com/iogamaster/achievable/Achievable.java
@@ -0,0 +1,8 @@
+package com.iogamaster.achievable;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Achievable implements ModInitializer {
+    @Override
+    public void onInitialize() {}    
+}
